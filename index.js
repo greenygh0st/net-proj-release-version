7a3938f98bcdfc7adc2ae288cf183b9eb7de48d0
jasondavis303 <jasondavis303@gmail.com>
2020-07-22 01:12:05 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 const fs = require('fs');
 const os = require("os");
+const parser = require('xml2json');
 
 function run()
 {
-    filename = process.env.PROJ_FILE || process.env.INPUT_PROJ_FILE;    
+    var filename = process.env.PROJ_FILE || process.env.INPUT_PROJ_FILE;    
     if (!fs.existsSync(filename))
         throw new Error('Project file not found');
 
     console.log(`Project File: ${filename}`)
 
-    rgx = new RegExp('\\<AssemblyVersion\\>\\(.*\\)\\<\\/AssemblyVersion\\>', 'm');
-    ver = rgx.exec(fs.readFileSync(filename, { encoding: 'utf-8' }))[1];
-
-    if (!ver)
-        throw new Error('Failed to get Assembly Version');
-
-    console.log(`Assembly Version: ${ver}`)
-
-    process.stdout.write(`::set-output name=ASSEMBLY_VERSION::${ver}` + os.EOL)
+    var xml = fs.readFileSync(filename, { encoding: 'utf-8' });
+    var json = parser.toJson(xml);
+    
+    console.log(json)
+    
+    
+    //process.stdout.write(`::set-output name=ASSEMBLY_VERSION::${ver}` + os.EOL)
 }
 
 run();
