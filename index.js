53f107395cb5af360e9e6e7064a8e871c1dec531
jasondavis303 <jasondavis303@gmail.com>
2020-07-22 01:18:55 -0600

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,8 @@ function run()
 
     console.log(`Project File: ${filename}`);
 
-    var xml = fs.readFileSync(filename, { encoding: 'utf-8' });
-    
     var rgx = new RegExp('\\<AssemblyVersion\\>(.*)\\<\\/AssemblyVersion\\>', 'm');
-    var ver = rgx.exec(fs.readFileSync(aip, { encoding: 'utf-8' }))[1];
+    var ver = rgx.exec(fs.readFileSync(filename, { encoding: 'utf-8' }))[1];
 
     if (!ver)
         throw new Error('Failed to get Assembly Version');
